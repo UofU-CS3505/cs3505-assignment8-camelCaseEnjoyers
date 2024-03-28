5fb0b4411957d14991e8ccdf5dfa90448d2128fd
jianjianh <142942690+jianjianh1@users.noreply.github.com>
2024-03-28 01:49:06 -0600

    Add model's functionalities related to drawing

--- SpriteEditor/model.cpp
+++ SpriteEditor/model.cpp
@@ -1,5 +1,49 @@
 #include "model.h"
 
 Model::Model(QObject *parent)
-    : QObject{parent}
-{}
+    : QObject(parent)
+    , sprite(defaultSpriteSize, backgroundColor)
+    , currentFrame(sprite.addFrame())
+    , currentTool(Tool::brush)
+    , currentColor()
+{
+}
+
+QImage *Model::newSprite(int size)
+{
+    sprite = Sprite(size, backgroundColor);
+    currentFrame = sprite.addFrame();
+    emit currentFrameChanged(currentFrame);
+    return currentFrame;
+}
+
+QImage *Model::newFrame()
+{
+    currentFrame = sprite.addFrame();
+    emit currentFrameChanged(currentFrame);
+    return currentFrame;
+}
+
+void Model::setTool(Tool tool)
+{
+    currentTool = tool;
+}
+
+void Model::setColor(QColor color)
+{
+    currentColor = color;
+}
+
+void Model::actionOnPixel(int x, int y)
+{
+    switch (currentTool) {
+    case Tool::brush:
+        currentFrame->setPixelColor(x, y, currentColor);
+        emit currentFrameChanged(currentFrame);
+        break;
+    case Tool::eraser:
+        currentFrame->setPixelColor(x, y, backgroundColor);
+        emit currentFrameChanged(currentFrame);
+        break;
+    }
+}
--- SpriteEditor/model.h
+++ SpriteEditor/model.h
@@ -1,7 +1,17 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "sprite.h"
+
 #include <QObject>
+#include <QColor>
+#include <QImage>
+
+enum class Tool
+{
+    brush,
+    eraser,
+};
 
 class Model : public QObject
 {
@@ -9,7 +19,27 @@ class Model : public QObject
 public:
     explicit Model(QObject *parent = nullptr);
 
+    inline QImage* getCurrentFrame() const { return currentFrame; }
+
 signals:
+    void currentFrameChanged(QImage *frame);
+
+public slots:
+    QImage *newSprite(int size);
+    QImage *newFrame();
+    void setTool(Tool tool);
+    void setColor(QColor color);
+    void actionOnPixel(int x, int y);
+
+private:
+    Sprite sprite;
+    QImage *currentFrame;
+    Tool currentTool;
+    QColor currentColor;
+
+public:
+    static constexpr int defaultSpriteSize = 32;
+    static inline const QColor backgroundColor = QColor(Qt::white);
 };
 
 #endif // MODEL_H
