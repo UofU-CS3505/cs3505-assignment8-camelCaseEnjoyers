9c37d4167f37112f0ac1f134768951fda25c613d
jianjianh <142942690+jianjianh1@users.noreply.github.com>
2024-03-28 01:50:19 -0600

    Add mouse event handler and image handler in canvas

--- SpriteEditor/canvas.cpp
+++ SpriteEditor/canvas.cpp
@@ -1,9 +1,30 @@
 #include "canvas.h"
 
 #include <QColor>
+#include <QMouseEvent>
+#include <QPixmap>
 
 Canvas::Canvas(QWidget* parent)
     : QLabel(parent)
     , image()
 {
+}
+
+void Canvas::processMouseEvent(QMouseEvent *ev)
+{
+    int canvasX = ev->pos().x();
+    int canvasY = ev->pos().y();
+    if (canvasX < 0 || canvasX >= width() || canvasY < 0 || canvasY >= height())
+    {
+        return;
+    }
+    int x = imageX(canvasX);
+    int y = imageY(canvasY);
+    emit mouseOnPixel(x, y);
+}
+
+void Canvas::setImage(QImage *image)
+{
+    this->image = image;
+    setPixmap(QPixmap::fromImage(image->scaled(width(), height())));
 }
--- SpriteEditor/canvas.h
+++ SpriteEditor/canvas.h
@@ -2,12 +2,29 @@
 #define CANVAS_H
 
 #include <QLabel>
+#include <QImage>
 
 class Canvas : public QLabel
 {
     Q_OBJECT
 public:
     Canvas(QWidget *parent = nullptr);
+
+protected:
+    inline virtual void mouseMoveEvent(QMouseEvent *ev) override { processMouseEvent(ev); }
+    inline virtual void mousePressEvent(QMouseEvent *ev) override { processMouseEvent(ev); }
+    inline virtual void mouseReleaseEvent(QMouseEvent *ev) override { processMouseEvent(ev); }
+
+private:
+    inline int imageX(int canvasX) const { return image->width() * canvasX / width(); }
+    inline int imageY(int canvasY) const { return image->height() * canvasY / height(); }
+    void processMouseEvent(QMouseEvent *ev);
+
+signals:
+    void mouseOnPixel(int x, int y);
+
+public slots:
+    void setImage(QImage* image);
 
 private:
     QImage *image;
