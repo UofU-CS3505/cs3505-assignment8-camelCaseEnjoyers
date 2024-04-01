80ff42ac34f2efb22fdd29fca1804cd811ab9a96
JoshGundersen <142958946+JoshGundersen@users.noreply.github.com>
2024-04-01 13:43:22 -0600

    Update previewanimation.h

--- SpriteEditor/previewanimation.h
+++ SpriteEditor/previewanimation.h
@@ -7,18 +7,19 @@
 namespace Ui {
     class previewAnimation;
 }
+
 // Sets up a class that will allow for the viewing of your different sprites in different frames per second. 
 class previewAnimation : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    explicit previewAnimation(Model *model,QWidget *parent = nullptr);
-    ~previewAnimation();
-
-private:
-    Ui::previewAnimation *ui;
-    Model *model;
+    public:
+        explicit previewAnimation(Model *model,QWidget *parent = nullptr);
+        ~previewAnimation();
+    
+    private:
+        Ui::previewAnimation *ui;
+        Model *model;
 };
 
 #endif // PREVIEWANIMATION_H
